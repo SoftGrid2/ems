f393f6c21024c7bac4db1281b43b2379fd80474e
SoftGrid2 <softgridinfo2@gmail.com>
2024-12-17 12:53:08 +0530

    update

--- auth-service/src/config/db.js
+++ auth-service/src/config/db.js
@@ -1,18 +1,21 @@
 require("dotenv").config();
 
-const { Client } = require("pg");
+const { Pool } = require("pg");
 
-const client = new Client({
+const pool = new Pool({
 	user: process.env.DB_USER,
 	host: process.env.DB_HOST,
 	database: process.env.DB_NAME,
 	password: process.env.DB_PASSWORD,
 	port: process.env.DB_PORT,
+	max: 20,
+	idleTimeoutMillis: 30000, // close idle clients after 30 seconds
+	connectionTimeoutMillis: 2000, // return an error after 2 seconds if connection can't be established
 });
 
-client
+pool
 	.connect()
 	.then(() => console.log("Connected to the database"))
 	.catch((err) => console.error("Connection error", err.stack));
 
-module.exports = client;
+module.exports = pool;
