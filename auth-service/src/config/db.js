f5fef48778b129822b998a40a9c9963224d4b84a
SoftGrid2 <softgridinfo2@gmail.com>
2024-12-19 16:27:59 +0530

    db string update

--- auth-service/src/config/db.js
+++ auth-service/src/config/db.js
@@ -3,10 +3,11 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-	connectionString:
-		process.env.DATABASE_URL ||
-		`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-	ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false, // SSL for Render
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	database: process.env.DB_NAME,
+	password: process.env.DB_PASSWORD,
+	port: process.env.DB_PORT,
 	max: 20,
 	idleTimeoutMillis: 30000,
 	connectionTimeoutMillis: 2000,
